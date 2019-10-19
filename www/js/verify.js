8704755a87cf2e33a2f2db326e7ef7674c62b386
Caleb Floyd <ctfloyd@wisc.edu>
2019-10-19 15:36:23 -0500

    test

--- www/js/verify.js
+++ www/js/verify.js
@@ -1,5 +1,47 @@
-$("#c-image").on("swipe", function(event) {
-    alert("It's a swipe!");
-});
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
 
-console.log("loaded");
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+            alert("left swipe");
+        } else {
+            /* right swipe */
+            alert("right swipe");
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            alert("up swipe");
+        } else { 
+            alert("down swipe");
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;  
+}
