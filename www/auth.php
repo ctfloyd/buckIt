2cb9cc668c9dbba67310c5a9a0687546021f71fd
root <root@lamp-1-vm.us-east4-b.c.buckit-256416.internal>
2019-10-20 04:02:13 +0000

    add auth.php

---
+++ www/auth.php
@@ -0,0 +1,27 @@
+<?php
+	if (isset($_POST['username']) && $_POST['username'] && isset($_POST['password']) && $_POST['password']) {
+	include("/var/www/dbOps.php");
+	if (testLogin($_POST['username'],$_POST['password'])) { 
+		// based on successful authentication
+		echo json_encode(array('success' => 1));
+	} else {
+		echo json_encode(array('success' => 0));
+	}
+	}
+
+
+
+	
+	/*
+	 * if $results['req'] == 'testLogin'{
+			$uname = $results['username'];
+		$pass = $results['password'];
+	
+		include "/var/www/dbOps.php";
+		if (testLogin($uname, $pass)) {
+			echo 'valid';
+		} else {
+			echo 'invalid'
+		}
+	}*/
+?>
